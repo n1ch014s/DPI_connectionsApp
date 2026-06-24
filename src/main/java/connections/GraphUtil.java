4b88855870262b2ca9dacafa1b8261fea8d70e50
Nicholas Salsi <n.salsi@stud.unibas.ch>
2026-06-24 16:33:23 +0200

    Add dummy files for transmission and GUI package; Add initial graph implementation in connections package.

---
+++ src/main/java/connections/GraphUtil.java
@@ -0,0 +1,121 @@
+import java.util.HashMap;
+
+
+public class GraphUtil {
+    public HashMap<PublicKey, Node> nodeList;
+    public Node userNode;
+
+    /**
+     * Constructor for GraphUtil.
+     * Creates User Node and intitializes nodeList of all Nodes.
+     *
+     * @param n User's name
+     * @param pub User's public key
+     * @param priv User's private key
+     */
+    GraphUtil(String userName, PublicKey pub, PrivateKey priv){
+        userNode = new Node(pub, userName, priv);
+        nodeList = new HashMap<PublicKey, Node>;
+        nodeList.put(pub, userNode);
+    }
+
+
+    /**
+     * Method for adding friends to a Node.
+     *
+     *
+     * @param node Existing Friend node to add friend to
+     * @param pub new friend's Public Key
+     */
+    public addFriendToFriend(Node node, PublicKey pub) {
+        if(nodeList.containsKey(pub)) {
+            node.addFriend(nodeList.get(pub));
+        }
+        else {
+            Node friend = new Node(pub);
+            nodeList.put(pub, friend);
+            node.addFriend(friend);
+        }
+    }
+
+    /**
+     * Method for adding list of friends to a Node.
+     *
+     *
+     * @param node Existing Friend node to add list of friends to
+     * @param pubKeys list of Public Keys of new friends
+     */
+    public addFriendToFriend(Node node, PublicKey[] pubKeys) {
+        for(int i = 0; i < pubKeys.length; i++) {
+            PublicKey pub = pubKeys[i];
+            if(nodeList.containsKey(pub)) {
+                node.addFriend(nodeList.get(pub));
+            }
+            else {
+                Node friend = new Node(pub);
+                nodeList.put(pub, friend);
+                node.addFriend(friend);
+            }
+        }
+    }
+
+    /**
+     * Method for adding friends to a Node by their Public Key.
+     *
+     *
+     * @param nodePub Existing Node's Public Key to add friend to
+     * @param friendPub new friend's Public Key
+     */
+    public addFriendToFriend(PublicKey nodePub, PublicKey friendPub) {
+        if(nodeList.containsKey(friendPub)) {
+            nodeList.get(nodePub).addFriend(nodeList.get(friendPub));
+        }
+        else {
+            Node friend = new Node(friendPub);
+            nodeList.put(friendPub, friend);
+            nodeList.get(nodePub).addFriend(friend);
+        }
+    }
+
+    /**
+     * Method for adding list of friends to a Node by their Public Key.
+     *
+     *
+     * @param nodePub Existing Node's Public Key to add list of friends to
+     * @param pubKeys list of Public Keys of new friends
+     */
+    public addFriendToFriend(PublicKey nodePub, PublicKey[] pubKeys) {
+        node = nodeList.get(nodePub);
+        for(int i = 0; i < pubKeys.length; i++) {
+            PublicKey pub = pubKeys[i];
+            if(nodeList.containsKey(pub)) {
+                node.addFriend(nodeList.get(pub));
+            }
+            else {
+                Node friend = new Node(pub);
+                nodeList.put(pub, friend);
+                node.addFriend(friend);
+            }
+        }
+    }
+
+    /**
+     * Method for adding friends to a Node.
+     *
+     *
+     * @param node Existing node to add friend to
+     * @param pub new friend's Public Key
+     */
+    public addFriendToUser(PublicKey pub, String name) {
+        if(nodeList.containsKey(pub)) {
+            Node friend = nodeList.get(pub);
+            friend.isFriend = true;
+            friend.name = name;
+            userNode.addFriend(friend);
+        }
+        else {
+            Node friend = new Node(pub, name);
+            userNode.addFriend(friend);
+        }
+    }
+}
---
+++ src/main/java/connections/Node.java
@@ -0,0 +1,84 @@
+import java.util.HashMap;
+
+
+public class Node {
+    //User Node attributes
+    public final boolean isUser;
+    public final PrivateKey privateKey;
+
+    //Friend Node attributes
+    public boolean isFriend;
+    public String name;
+
+    //General attributes
+    public final PublicKey publicKey;
+    public HashMap<PublicKey, Node> friends;
+
+    /**
+     * Constructor for User Node
+     *
+     * @param pub User's public key
+     * @param priv User's private key
+     * @param n User's name
+     */
+    Node(PublicKey pub, String n, PrivateKey priv) {
+        isUser = true;
+        isFriend = false;
+        publicKey = pub;
+        privateKey = priv;
+        name = n;
+        friends = new HashMap<PublicKey, Node>;
+    }
+
+    /**
+     * Constructor for Friend Node
+     *
+     * @param pub Friend's public key
+     * @param n Friend's name
+     */
+    Node(PublicKey pub, String n) {
+        isUser = false;
+        isFriend = true;
+        publicKey = pub;
+        name = n;
+        friends = new HashMap<PublicKey, Node>;
+    }
+
+    /**
+     * Constructor for Friend of Friend Node
+     *
+     * @param pub User's public key
+     */
+    Node(PublicKey pub) {
+        isUser = false;
+        isFriend = false;
+        publicKey = pub;
+        friends = new HashMap<PublicKey, Node>;
+    }
+
+    /**
+     * Method for adding new Friend node to this node's friend map.
+     * Also adds this node as new Friend to other node.
+     *
+     * @param node new Friend node
+     */
+    default addFriend(Node friendNode) {
+        friends.put(friendNode.publicKey, friendNode);
+        friendNode.friends.put(publicKey, this);
+    }
+
+    /**
+     * Method for removing Friend node from this node's friend map.
+     * Also removes this node as Friend from other node.
+     *
+     * @param node Friend node to be removed
+     */
+    default removeFriend(Node friendNode) {
+        friends.remove(friendNode.publicKey);
+        friendNode.friends.remove(publicKey);
+        if(friendNode.isFriend && isUser) {
+            friendNode.isFriend = false;
+            friendNode.name = NULL;
+        }
+    }
+}
